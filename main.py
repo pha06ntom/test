1e1798cbaca8a296c761037a210cbda2c44a9c25
arispi <pha06ntom@mail.ru>
2023-11-15 20:28:28 +0300

    telebot

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from telebot import types,telebot
+
+bot = telebot.TeleBot("token")  #вставить token из телеграмма
+
+db = {'ФИО': 'Иванов Иван', 'Адрес': 'ул.Ленина, д.7', 'Работа': 'Школа №34'}
+
+@bot.message_handler(commands=["start"]) #управитель сообщениями
+def start(m, res=False): # m - сообщение, res - отдых
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Кнопки
+    item1 = types.KeyboardButton("ФИО")
+    item2 = types.KeyboardButton("Адрес")
+    item3 = types.KeyboardButton("Работа")
+
+    markup.add(item1)
+    markup.add(item2)
+    markup.add(item3)
+
+    bot.send_message(m.chat.id, f'Здравствуйте, что Вас интересует', reply_markup=markup)  #сообщение при входе
+
+@bot.message_handler(content_types=["text"])
+def handler_text(message):
+
+    if message.text.strip() == 'ФИО':
+        answer = db['ФИО']
+    elif message.text.strip() == 'Адрес':
+        answer = db['Адрес']
+    elif message.text.strip() == 'Работа':
+        answer = db['Работа']
+    else:
+        ansxer = 'Такой команды нет'
+    bot.send_message(message.chat.id, answer)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+bot.polling(none_stop=True, interval=0)   # Запуск бота без остановочно
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("The end")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
